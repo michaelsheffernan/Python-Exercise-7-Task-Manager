09145243d084a759f595c92500628caa4975cdb2
Michael Sweeney Heffernan <sweeneym404@gmail.com>
2026-05-01 09:56:31 +0100

    Add files via upload
    Created a class object for the task manager with task functions that I then imported the methods into my main task manager file for reusability.

--- Python Exercise-Task Manager.py
+++ Python Exercise-Task Manager.py
@@ -1,54 +1,12 @@
+from Tasks import *
+
 print("""Welcome to Task Manager 
 Please Sign Up""")
 
 user_email = input("Email: ")
 user_password = input("Password: ")
 
-
-task_list = []
-
-
-def show_tasks():
-    if task_list == []:
-        print("You currently have no tasks.")
-
-    else:
-        print(f"Your current tasks are: {task_list}")
-    return
-
-
-def add_tasks(task_list):
-    print("Add a Task")
-    new_task = str(input("New Task: "))
-    task_list.append(new_task)
-    print(f"You have added {new_task} to your task list.")
-    return task_list
-
-
-def delete_tasks(task_list):
-    print("Type Task for deletion")
-    deleted_task = input("Delete Task: ")
-    deletion_confirmation = input("Confirm deletion y/n: ")
-    if deletion_confirmation == "y":
-        task_list.remove(deleted_task)
-        print(f"You have deleted {deleted_task} from your current tasks.")
-        return task_list
-    elif deletion_confirmation == "n":
-        print("Deletion Cancelled")
-        return task_list
-    else:
-        print("Invalid Input")
-        return task_list
-
-
-def mark_task(task_list):
-    print("Which Task should be marked complete?")
-    marked_task = input("Mark Task Complete: ")
-    task_list.remove(marked_task)
-    task_list.append(marked_task + " ✅ ")
-    print("Task marked complete")
-    return task_list
-
+task_manager = Tasks()
 
 log_in_attempt = 0
 
@@ -62,7 +20,7 @@ while log_in_attempt < 3:
         print("Welcome")
 
         while True:
-            print(""" TASK MANAGER"
+            print(""" TASK MANAGER
                 -----------------
                 1. Show Tasks
                 2. Add Tasks
@@ -71,24 +29,18 @@ while log_in_attempt < 3:
                 5. Exit """)
             user_input = input("Enter command: ")
             if user_input == "1":
-                show_tasks()
+                task_manager.show_tasks()
 
             elif user_input == "2":
-                task_list = add_tasks(task_list)
+                task_manager.add_tasks()
 
             elif user_input == "3":
-                try:
-                    task_list = delete_tasks(task_list)
 
-                except ValueError:
-                    print("That task is not in your list")
+                task_manager.delete_tasks()
 
             elif user_input == "4":
-                try:
-                    task_list = mark_task(task_list)
 
-                except ValueError:
-                    print("That task is not in your task list.")
+                task_manager.mark_task_complete()
 
             elif user_input == "5":
                 print("Thank you for using Task Manager")
